a62c8ebe367df894d79c03143f841da2fb314990
Dylane <tanolamene@gmail.com>
2025-01-19 14:00:36 -0400

    Add chat room functionality with user joining and message broadcasting
    End step 3

--- server.js
+++ server.js
@@ -14,22 +14,49 @@ app.get("/", (req, res) => {
   res.send("Serveur socket.io fonctionne !");
 });
 
+// Etat du serveur : Stocker les utilisateurs et leurs rooms
+const users = {};
+
 // Gerer les connexions Socket.IO
 //socket est un objet represente une connection unique entre le serveur et ce client specifique
 // on egalement un id avec socket.id, une nouvel instance de cet objet est creer a chaque fois qu'un user ce connect
 io.on("connection", (socket) => {
   console.log(`L'utilisateur ${socket.id}, s'est connecte ✅`);
 
-  // Ecouter un evenement personnaliser
-  socket.on("message", (data) => {
-    console.log("Message recu : ", data);
+  //Auand un utilisateur rejoint une room
+  socket.on("joinRoom",({username, room}) => {
+    // Enregistrer l'utilisateur
+    users[socket.id] = {username, room};
+    socket.join(room) // ajouter l'utilisateur a la room
 
-    // Repondre a tous les client connectes
-    io.emit("message", `Message diffuse : ${data}`);
+    // notification aux autres utilisateurs de la room
+    socket.to(room).emit("notification", `${username} a rejoint la room ${room}`)
+    console.log(`${username} a rejoint la room ${room}`);
+  })
+
+  // recuperer le message
+  socket.on("message", (message) => {
+    const user = users[socket.id];
+    if(user && user.room){
+
+      // Envoyer le message uniquement a la room
+      io.to(user.room).emit("message", {username: user.username, message})
+    }
   });
 
   // Gerer la deconnexion
   socket.on("disconnect", () => {
+    const user = users[socket.id];
+    if(user){
+      const {username, room} = user;
+
+      // Supprimer l'utilisateur de la liste
+      delete users[socket.id];
+
+      // notifier les autres utilisateurs de la room 
+      socket.to(room).emit("notification", `${username} a quitte la room.`);
+      console.log(`${username} a quitte la room ${room}.`)
+    }
     console.log(`L'utilisateur ${socket.id}, s'est deconnecte ❌.`);
   });
 });
